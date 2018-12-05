c5d431b34fbca6f6606dbc9b535acbd6d4a9f21a
Jorge Ribeiro <joorgemelo@gmail.com>
2018-12-05 19:44:54 -0300

    modified:   .gitignore 	modified:   filters_visualization.py 	modified:   validate_model.py

--- validate_model.py
+++ validate_model.py
@@ -12,22 +12,10 @@ from quiver_engine.server import launch
 # Comment/uncomment to select dataset to use
 dataset = DatasetLoader()
 
-# JAFFE
-# dataset.jaffe_load_from_save()
-# classes = JAFFE_EMOTIONS
-# num_classes = len(classes)
-
 # CK Extended
 dataset.ck_extended_load_from_save()
 classes = CK_EXTENDED_EMOTIONS
 num_classes = len(classes)
-
-# FERPlus (select one)
-# dataset.fer_2013_small_load_from_save() # SMALL
-# dataset.fer_2013_medium_load_from_save() # MEDIUM
-# dataset.fer_2013_load_from_save() # FULL
-# classes = FER_2013_EMOTIONS
-# num_classes = len(classes)
 
 x_train, x_test = dataset.images, dataset.images_test
 y_train, y_test = dataset.labels, dataset.labels_test
@@ -36,7 +24,7 @@ x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
 # Load model
-model = load_model(MODELS_PATH + 'model_ck_extended_vgg_16_1.h5')
+model = load_model(MODELS_PATH + 'model_ck_extended_vgg_16_2.h5')
 print ('[+] Model loaded')
 
 count = 0
