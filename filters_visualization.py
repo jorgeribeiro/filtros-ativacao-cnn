7951050767781930cafbd44aeb5ed1c8407903d5
Jorge Ribeiro <joorgemelo@gmail.com>
2018-12-05 19:51:26 -0300

    modified:   filters_visualization.py 	modified:   validate_model.py

--- filters_visualization.py
+++ filters_visualization.py
@@ -16,12 +16,12 @@ img_height = 32
 # Comment/uncomment to select dataset to use
 dataset = DatasetLoader()
 
-# CK Extended no resize
+# CK Extended
 dataset.ck_extended_load_from_save()
 classes = CK_EXTENDED_EMOTIONS
 
 # Load model
-model = load_model(MODELS_PATH + 'model_ck_extended_vgg_16_2.h5')
+model = load_model(MODELS_PATH + 'model_ck_extended_inception_v3_1.h5')
 print ('[+] Model loaded')
 
 from quiver_engine.server import launch
--- validate_model.py
+++ validate_model.py
@@ -24,7 +24,7 @@ x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
 # Load model
-model = load_model(MODELS_PATH + 'model_ck_extended_vgg_16_2.h5')
+model = load_model(MODELS_PATH + 'model_ck_extended_inception_v3_1.h5')
 print ('[+] Model loaded')
 
 count = 0
